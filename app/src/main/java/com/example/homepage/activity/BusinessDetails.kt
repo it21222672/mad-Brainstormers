0e4979bb35b846a24addc9d934d316b21f19d3cb
Praveen Liyanage <99320096+IT21327780@users.noreply.github.com>
2023-05-10 17:06:35 +0530

    Initial commit

--- app/src/main/java/com/example/homepage/activity/BusinessDetails.kt
+++ app/src/main/java/com/example/homepage/activity/BusinessDetails.kt
@@ -1,5 +1,6 @@
 package com.example.homepage.activity
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/homepage/activity/uploading.kt
+++ app/src/main/java/com/example/homepage/activity/uploading.kt
@@ -1,11 +1,18 @@
 // Declare package name and import required libraries
 package com.example.homepage.activity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.example.homepage.Dashboard
 import com.example.homepage.R
 
@@ -17,7 +24,31 @@ class uploading : AppCompatActivity() {
 
 
     // Override the onCreate method
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Create a notification channel (required on Android 8.0 and above)
+        val channelId = "my_channel_id"
+        val channelName = "My Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        // Create an intent for the button action
+        val intent = Intent(this, uploading::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Build the notification
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(androidx.core.R.drawable.notification_icon_background)
+            .setContentTitle("Submission Received")
+            .setContentText("Thank you for submitting your details!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // Send the notification
+        val notificationId = 1
+        notificationManager.notify(notificationId, builder.build())
 
         // Set the system UI visibility to fullscreen
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
